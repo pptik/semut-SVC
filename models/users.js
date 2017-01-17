6048b2de447b1da44cd4c948ea570c62531ae09f
Hendra Permana <hynra@yahoo.com>
2017-01-17 17:08:46 +0700

    User login [ok]

--- models/users.js
+++ models/users.js
@@ -6,7 +6,6 @@ db = app.db;
 exports.login = function (query, callback) {
     db.collection('tb_user', function(err, collection) {
         collection.find({"Email": query['Email']}).toArray(function(err, items) {
-         //   console.log(items);
             if(err){
                 callback(err, null);
             }else {
@@ -16,17 +15,60 @@ exports.login = function (query, callback) {
                             if(err){
                                 callback(err, null);
                             }else {
-                                collection.find({"UserID": items[0].ID, "EndTime": "0000-00-00 00:00:00"
+                                var usr_id = items[0].ID;
+                                collection.find({"UserID": usr_id, "EndTime": "0000-00-00 00:00:00"
                                 }).toArray(function(err, items) {
                                     if(err){
                                         callback(null, err);
                                     }else {
-                                        //console.log(items);
                                         if(items[0]){
-
-                                        }else {
-
+                                            collection.updateOne({ID: items[0].ID},{ $set: { EndTime : moment().format('YYYY-MM-DD HH:mm:ss')}}, function(err, result) {
+                                                if(err){
+                                                    callback(err, null);
+                                                }
+                                            });
                                         }
+                                        var _query = {UserID: usr_id, ID: usr_id+"-"+moment().format('YYYYMMDDHHmmss'),StartTime: moment().format('YYYY-MM-DD HH:mm:ss'), LastTime:moment().format('YYYY-MM-DD HH:mm:ss'), EndTime: "0000-00-00 00:00:00"}
+                                        collection.insertOne(_query, function (err, result) {
+                                            if (err) {
+                                                callback(err, null);
+                                            } else {
+                                                db.collection('tb_user', function(err, collection) {
+                                                    collection.find({"Email": query['Email']}).toArray(function(err, items) {
+                                                        if(err){
+                                                            callback(err, null);
+                                                        }else {
+                                                            var profile = items[0];
+                                                            delete profile['_id'];
+                                                            delete profile['Password'];
+                                                            delete profile['flag'];
+                                                            delete profile['Barcode'];
+                                                            delete profile['ID_role'];
+                                                            delete profile['Plat_motor'];
+                                                            delete profile['ID_ktp'];
+                                                            delete profile['PushID'];
+                                                            delete profile['Status_online'];
+                                                            delete profile['Nama_foto'];
+                                                            delete profile['Path_ktp'];
+                                                            delete profile['Nama_ktp'];
+                                                            delete profile['VerifiedNumber'];
+                                                            db.collection('tb_session', function (err, collection) {
+                                                                collection.find({
+                                                                    "UserID": usr_id,
+                                                                    "EndTime": "0000-00-00 00:00:00"
+                                                                }).toArray(function (err, items) {
+                                                                    if(err){
+                                                                        callback(err, null);
+                                                                    }else {
+                                                                        callback(null, {success: true, message:"Berhasil melakukan login", sessID: items[0]['ID'], profile: profile});
+                                                                    }
+                                                                });
+                                                            });
+                                                        }
+                                                    });
+                                                });
+                                            }
+                                        });
                                     }
                                 });
                             }
