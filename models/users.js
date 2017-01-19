2002d4f4b8378729bad98d3cd335b229d6d51c02
Hendra Permana <hynra@yahoo.com>
2017-01-19 12:17:14 +0700

    User signup [ok]

--- models/users.js
+++ models/users.js
@@ -1,6 +1,7 @@
 app = require('../app');
 var md5 = require('md5');
 var moment 	= require('moment');
+var autoIncrement = require("mongodb-autoincrement");
 db = app.db;
 
 exports.login = function (query, callback) {
@@ -82,4 +83,73 @@ exports.login = function (query, callback) {
             }
         });
     });
+};
+
+exports.regsiter = function (query, callback) {
+    var email = query.Email;
+    var phonenumber = query.Phonenumber;
+    var gender = query.Gender;
+    var birthday = query.Birthday;
+    var password = query.Password;
+    var name = query.Name;
+
+    var userColl = db.collection('tb_user');
+    if(userColl){
+        userColl.find({"Email": email}).toArray(function(err, items) {
+            if(items[0]) {
+                callback(null, {success: false, message: "Email sudah digunakan!"});
+            }else {
+                autoIncrement.getNextSequence(db, 'tb_user', function (err, autoIndex){
+                    if(err){
+                        console.log(err);
+                        callback(err, null);
+                    }else {
+                        var userQuery = {
+                            "ID" : autoIndex,
+                            "Name" : name,
+                            "Email" : email,
+                            "CountryCode" : 62,
+                            "PhoneNumber" : phonenumber,
+                            "Gender" : gender,
+                            "Birthday" : birthday,
+                            "Password" : md5(password),
+                            "Joindate" : moment().format('YYYY-MM-DD HH:mm:ss'),
+                            "Poin" : 100,
+                            "PoinLevel" : 100,
+                            "AvatarID" : gender,
+                            "facebookID" : null,
+                            "Verified" : 0,
+                            "VerifiedNumber" : null,
+                            "Visibility" : 0,
+                            "Reputation" : 0,
+                            "flag" : 1,
+                            "Barcode" : "",
+                            "deposit" : 0,
+                            "ID_role" : null,
+                            "Plat_motor" : null,
+                            "ID_ktp" : null,
+                            "foto" : null,
+                            "PushID" : "no id",
+                            "Status_online" : null,
+                            "Path_foto" : null,
+                            "Nama_foto" : null,
+                            "Path_ktp" : null,
+                            "Nama_ktp" : null
+                        };
+                        userColl.insertOne(userQuery, function (err, result) {
+                            if(err){
+                                console.log(err);
+                                callback(err, null);
+                            }else {
+                                console.log(result.ops);
+                                callback(null, {success:true, message: "berhasil membuat akun, silahkan login"});
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }else {
+        callback("server error", null);
+    }
 };
--- routes/users.js
+++ routes/users.js
@@ -20,4 +20,25 @@ router.post('/login', function(req, res, next) {
   }
 });
 
+router.post('/signup', function(req, res, next) {
+  console.log(req.body);
+  var email = req.body.Email;
+  var phonenumber = req.body.Phonenumber;
+  var gender = req.body.Gender;
+  var birthday = req.body.Birthday;
+  var password = req.body.Password;
+  var name = req.body.Name;
+  if(email == null || phonenumber == null || gender == null || birthday == null || password == null || name == null) {
+    res.status(200).send({success: false, message: "parameter tidak lengkap"});
+  }else {
+    userModel.regsiter(req.body, function (err, result) {
+      if(err){
+        res.status(200).send({success: false, message: "Server bermasalah"});
+      }else {
+        res.status(200).send(result);
+      }
+    });
+  }
+});
+
 module.exports = router;
