64a541cf51466dcaa6092632761171b8d1c80f11
Hendra Permana <hynra@yahoo.com>
2017-01-19 16:32:05 +0700

    Friend request [ok]

---
+++ routes/friend.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var friendModel = require('../models/friend');
+
+router.post('/request', function(req, res, next) {
+    console.log(req.body);
+    if(req.body['SessionID'] == null || req.body['UserID'] == null) {
+        res.status(200).send({success: false, message: "parameter tidak lengkap"});
+    }else {
+        // res.status(200).send()
+        friendModel.request(req.body, function (err, result) {
+            if(err){
+                res.status(200).send({success: false, message: "Server bermasalah"});
+            }else {
+                res.status(200).send(result);
+            }
+        });
+    }
+});
+
+
+module.exports = router;
