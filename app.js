24e7393b3e925ee377c6ccfa506183baa7088783
Hendra Permana <hynra@yahoo.com>
2017-02-09 12:39:09 +0700

    update place collection

--- app.js
+++ app.js
@@ -106,6 +106,9 @@ database.connect(function (err, db) {
         app.use('/api/friend', friend);
         app.use('/api/location', location);
         app.use('/api/post', post);
+
+        var placeModel = require('./model/place_model');
+        placeModel.fixPlace();
         app.use(function(req, res, next) {
             var err = new Error('Not Found');
             err.status = 404;
